ba0a5cffcea7abcd40bb86db0d90992bbad074c4
AlptGames <alptgames@gmail.com>
2026-04-05 14:35:07 +0300

    comitusik

--- Newton vs apple/Assets/Scripts/Ads.cs
+++ Newton vs apple/Assets/Scripts/Ads.cs
@@ -1,39 +1,59 @@
-/*using UnityEngine;
-using YG; // ��������� ���� namespace
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
 
 public class Ads : MonoBehaviour
 {
+    public string idAdv;
+
     public Hero script;
-
-    private void Start()
+    
+    void Start()
     {
-        script = GetComponent<Hero>();
+      script = GetComponent<Hero>();
     }
 
-    private void OnEnable()
+    void Update()
     {
-        // ������������� �� ������� (����� ������ ��������� string)
+       //script.moneyText.text = script.FormatNumber(script.money) + " $";
+    }
+
+      private void OnEnable()
+      {
         YG2.onRewardAdv += Rewarded;
-    }
+      } 
 
-    private void OnDisable()
-    {
-        // ����������� ������������, ����� �������� ������ ������
+      private void OnDisable()
+      {
         YG2.onRewardAdv -= Rewarded;
-    }
+      }
 
-    // ���������� �������
-    void Rewarded(string id)
-    {
-        // ���� � ��� ��������� ����� ������, ����� ��������� id
-        script.money += 500;
-        Debug.Log("����� ������� �������!");
-    }
+      private void Rewarded(string id)
+      {
+        if(id == idAdv)
+        {
+            SetReward();
+            
+        }
+      } 
 
-    // ����� ��� ������ ����� ������� (��������� � ������)
-    public void ShowAd()
-    {
-        // ����� ���� � �������� �� ��������������
-        YG2.RewardedAdvShow("�������_1");
-    }
-}*/
+      public void SetReward()
+      {
+
+      //Hero.currentLives = 1;
+
+      }
+
+      public void ShowRewardAdv_UseCallback()
+      {
+        YG2.RewardedAdvShow(idAdv, () =>
+        {
+
+            SetReward();
+        });
+      }
+   
+    
+}
